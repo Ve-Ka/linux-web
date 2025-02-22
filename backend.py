ee2a4205f01284ea698f5f407c76266c8c2f9a31
VeKa <ver_jj@yahoo.com>
2025-02-23 03:56:02 +0800

    change to and for better detection

--- backend.py
+++ backend.py
@@ -98,7 +98,7 @@ def start_container():
             print(f"Error in function '{function_name}': {e}")
 
     # Different command for Debian/Ubuntu to install ttyd
-    if "alpine" not in distro or "alpine" not in version:
+    if "alpine" not in distro and "alpine" not in version:
         command = (
             "sh -c 'apt update && apt install -y curl && "
             "curl -Lo /usr/local/bin/ttyd https://github.com/tsl0922/ttyd/releases/latest/download/ttyd.x86_64 && "
